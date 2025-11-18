9019878560b6e8571fe3c6c6032ccea5224b161b
ayanganem55-source <ayanganem55@gmail.com>
2025-11-18 20:30:58 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,37 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+
+    parts = []
+    current_part = ""
+
+    for ch in formula:
+        if ch.isupper() and current_part:
+
+            parts.append(current_part)
+            current_part = ch
+        else:
+            current_part += ch
+
+
+        if current_part:
+            parts.append(current_part)
+
+    return parts
+
 
 
 def split_at_first_digit(formula):
-    pass # Replace the `pass` with your code
+    digit_location = 1
+
+    for ch in formula[1:]:
+        if ch.isdigit():
+            break   
+        digit_location += 1
+
+
+    if digit_location == len(formula):
+        return formula, 1
+        
+    prefix = formula[:digit_location]
+    number = int(formula[digit_location:])
+    return prefix, number
+
